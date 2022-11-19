7a1b781f80c428a831246083c014164e59922f2a
linosk <linakoskar@gmail.com>
2022-11-19 23:49:53 +0100

    Working quicksort implemented, the names of the funcion can be changed

--- main.c
+++ main.c
@@ -2,29 +2,25 @@
 #include <stdio.h>
 #include <time.h>
 
-#include "bubble_sort.h"
+//#include "bubble_sort.h"
+#include "quick_sort.h"
 
 int main(int argc, char** argv)
 {
+    int arr[15] = {9,3,7,4,1,5,6,1,6,2,1,0,8,5,4};
+    print_int_array(arr,15);
+    quick_sort(arr,15);
+    print_int_array(arr,15);
 
-    //int len = 9;
-    //int* arr = malloc(len*sizeof(int));
+    //int arr[9] = {3,7,4,6,2,1,0,8,5};
+    //print_int_array(arr,9);
+    //quick_sort(arr,9);
+    //print_int_array(arr,9);
 
-    int arr[9] = {9,8,7,6,5,4,3,2,1};
-
-    //srand(time(0));
-    //for(int i=0;i<len;i++)
-    //{
-    //    arr[i] = rand()%9;
-    //}
-
-    print_int_array(arr,9);
-    bubble_sort(arr,9);    
-    print_int_array(arr,9);
-
-
-
-    //free(arr);
+    //int arr[5] = {9,7,1,3,5};
+    //print_int_array(arr,5);
+    //quick_sort(arr,5);
+    //print_int_array(arr,5);
 
     return 0;
 }
---
+++ quick_sort.h
@@ -0,0 +1,9 @@
+#ifndef __QUICK_SORT_H__
+#define __QUICK_SORT_H__
+
+#include "usefull.h"
+
+void sort(int* array, int first, int last);
+void quick_sort(int* array, int length);
+
+#endif
