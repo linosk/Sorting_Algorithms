e5cdfb78ea99356f4da1107426051ebbb6737780
linosk <linakoskar@gmail.com>
2022-12-02 19:28:31 +0100

    Quick sort implemented(better way)

--- quick_sort.c
+++ quick_sort.c
@@ -1,31 +1,40 @@
 #include "quick_sort.h"
 
+int __pivot(int* array, int first, int last)
+{
+    int li = first;
+    int ri = last;
+    int pi = last;
+    while(li<ri)
+    {
+        if(array[li]>array[pi])
+        {
+            if(array[ri]<array[pi])
+            {
+                swap_int_array_elements(&array[li],&array[ri]);
+            }
+            else
+                ri--;
+        }
+        else
+            li++;
+    }
+    swap_int_array_elements(&array[ri],&array[pi]);
+
+    return ri;
+}
+
 void __sort(int* array, int first, int last)
 {
     if(first<last)
     {
-        int li = first;
-        int pi = last-1;
-        int ri = last-1;
-        while(li<ri)
-        {
-            if(array[li]>array[pi])
-            {
-                if(array[ri]<array[pi])
-                    swap_int_array_elements(&array[li],&array[ri]);
-                else
-                    ri--;
-            }
-            else
-                li++;
-        }
-        swap_int_array_elements(&array[ri],&array[pi]);
-        __sort(array,first,ri);
-        __sort(array,ri+1,last);
+        int pivot = __pivot(array,first,last);
+        __sort(array,first,pivot-1);
+        __sort(array,pivot+1,last);
     }
 }
 
 void quick_sort(int* array, int length)
 {
-    __sort(array,0,length);
+    __sort(array,0,length-1);
 }
--- quick_sort.h
+++ quick_sort.h
@@ -3,7 +3,11 @@
 
 #include "usefull.h"
 
-void sort(int* array, int first, int last);
+//Partition should returns a pivot, quick_sort should just be a function that is being called first, then a function that
+//is called recursively, partition is called in meantime and again it retuirns pivot/pivot index
+
+int __pivot(int* array, int first, int last);
+void __sort(int* array, int first, int last);
 void quick_sort(int* array, int length);
 
 #endif
